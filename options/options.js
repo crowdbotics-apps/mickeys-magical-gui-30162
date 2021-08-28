1a6c82328b43d65b03ac741785ff056edaebe70b
Crowdbotics <team@crowdbotics.com>
2021-08-28 16:03:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mickeys_magical_gui_30162Identifier",
+  url: "https://mickeys_magical_gui_30162Identifier.botics.co",
+  api: "https://mickeys_magical_gui_30162Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
